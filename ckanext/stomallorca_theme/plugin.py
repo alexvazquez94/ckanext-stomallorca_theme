10dbc8db76ce3ee556945ec40121d01116548184
Miguel Angel <machuecos@disid.com>
2023-08-11 11:41:13 +0200

    ee

--- ckanext/stomallorca_theme/plugin.py
+++ ckanext/stomallorca_theme/plugin.py
@@ -3,7 +3,6 @@ import ckan.plugins.toolkit as toolkit
 from ckan.lib.plugins import DefaultTranslation
 from ckan.common import config
 from ckan.model import Package
-import ckan.lib.helpers as h
 
 def default_locale():
     '''Wrap the ckan default locale in a helper function to access
@@ -61,7 +60,7 @@ class StomallorcaThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
             'stomallorca_portal_url': stomallorca_portal_url,
             'stomallorca_theme_get_license': get_license,
             'stomallorca_theme_get_group': get_group,
-            'fluent_core_title_translated': h.get_template('extrafields_fluent_title.html')
+            'fluent_core_title_translated': toolkit.get_template('extrafields_fluent_title.html')
         }
     
     # IPackageController
