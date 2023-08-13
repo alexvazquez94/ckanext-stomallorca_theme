526b46413f1c2a6392cb8f5dbc5a77829cab6a31
Miguel Angel <machuecos@disid.com>
2023-08-13 19:28:04 +0200

    dd

--- ckanext/stomallorca_theme/plugin.py
+++ ckanext/stomallorca_theme/plugin.py
@@ -5,8 +5,6 @@ from ckan.common import config
 from ckan.model import Package
 import ckan.lib.helpers as h
 
-from ckanext.stomallorca_theme import validators, helpers
-
 def default_locale():
     '''Wrap the ckan default locale in a helper function to access
     in templates.
@@ -43,25 +41,15 @@ def get_group(group_id):
 class StomallorcaThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
     plugins.implements(plugins.ITranslation)
     plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.IValidators, inherit=True)
     plugins.implements(plugins.ITemplateHelpers)
     plugins.implements(plugins.IPackageController, inherit=True)
 
     # IConfigurer
+
     def update_config(self, config_):
         toolkit.add_template_directory(config_, "templates")
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("assets", "stomallorca_theme")
-
-    # #########################################################################
-    # #########################################################################
-    # IValidators
-    # #########################################################################
-    # #########################################################################
-    def get_validators(self):
-        return {            
-            'tags_html_detected': validators.tags_html_detected,
-            }
 
     # ITemplateHelpers
     def get_helpers(self):
@@ -72,8 +60,7 @@ class StomallorcaThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
             'extrafields_default_locale': default_locale,
             'stomallorca_portal_url': stomallorca_portal_url,
             'stomallorca_theme_get_license': get_license,
-            'stomallorca_theme_get_group': get_group,
-            'stomallorca_multiple_field_required': helpers.stomallorca_multiple_field_required,
+            'stomallorca_theme_get_group': get_group
         }
     
     # IPackageController
